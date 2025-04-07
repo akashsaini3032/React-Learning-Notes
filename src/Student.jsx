8cb347615cb550665693021acb52901592a3dab2
akashsaini3032 <akashsainira@gmail.com>
2025-04-07 22:51:25 +0530

    update content

---
+++ src/Student.jsx
@@ -0,0 +1,10 @@
+const Student=(props)=>{
+    return(
+        <>
+        <h1>Hello iam {props.nm}</h1>
+        <h2>My City is {props.city}</h2>
+        <h2>I learn {props.subject}</h2>
+        </>
+    )
+}
+export default Student;
