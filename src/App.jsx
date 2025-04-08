728709bfc9183f1726754c6edde083a484f5de44
akashsaini3032 <akashsainira@gmail.com>
2025-04-08 23:09:43 +0530

    update content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,88 @@
-import Student from "./Student";
-const App = () => {
-  return (
+const Student = [
+  {
+    "rollno":124,
+    "name":"viplove",
+    "city":"bhopla",
+    "fees":5467567
+
+},
+{
+  "rollno":124,
+  "name":"jkdbcj",
+  "city":"bhopla",
+  "fees":5467567
+
+},
+{
+  "rollno":124,
+  "name":"vbjkbcjk",
+  "city":"bhopla",
+  "fees":5467567
+
+},
+{
+  "rollno":124,
+  "name":"shubham",
+  "city":"bhopla",
+  "fees":5467567
+
+},
+{
+  "rollno":127,
+  "name":"ankit",
+  "city":"bhopal",
+  "fees":5467567
+
+},
+{
+  "rollno":798,
+  "name":"rohan",
+  "city":"indore",
+  "fees":5467567
+
+},
+]
+
+
+const ans = Student.map((key)=>{
+  return(
     <>
-      <h1 align="center">this is Prop class</h1>
-      <Student nm="akash" city="bhopal" subject="React js" />
+    <tr>
+      <td>{key.rollno}</td>
+      <td>{key.name}</td>
+      <td>{key.city}</td>
+      <td>{key.fees}</td>
+
+
+    </tr>
+    
     </>
-  );
-};
+  )
+
+  
+})
+
+const App = ()=>{
+ 
+  return(
+  <>
+ <h1 align="center">welcome to cybrom</h1>
+
+ <>
+ 
+ <table border="2" width="600" height='400'>
+  <tr>
+    <th>rollno</th>
+    <th>name</th>
+    <th>city</th>
+    <th>fees</th>
+    
+  </tr>
+  {ans}
+ </table>
+ </>
+
+  </>
+  )
+}
 export default App;
