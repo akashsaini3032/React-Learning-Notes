f8a65d474ba7fcb33b74eda31aa266ec9e107e91
akashsaini3032 <akashsainira@gmail.com>
2025-04-09 10:05:45 +0530

    upate contetn

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-// const App = ()=>{
-//   return(
-//       <>
-//     <div style={{border:"3px solid red" , width:"600px", height:"400px", margin:"auto",borderRadius:"20px" ,color:"red",backgroundColor:"blue",color:"white"}}>
-//       <div style={{border:"3px solid blue" , width:"400px", height:"300px", margin:"auto",borderRadius:"20px",alignItems:"center",backgroundColor:"yellow",marginTop:"50px"}}>
-//         <div style={{border:"3px solid green" , width:"300px", height:"200px", margin:"auto",borderRadius:"20px",alignItems:"center",backgroundColor:"red",marginTop:"45px"}}>
-//           <h1 style={{marginLeft:"90px", marginTop:"70px"}}>Cybrpm</h1>
-//         </div>
-//       </div>
-//     </div>
+const App = ()=>{
+  return(
+      <>
+    <div style={{border:"3px solid red" , width:"600px", height:"400px", margin:"auto",borderRadius:"20px" ,color:"red",backgroundColor:"blue",color:"white"}}>
+      <div style={{border:"3px solid blue" , width:"400px", height:"300px", margin:"auto",borderRadius:"20px",alignItems:"center",backgroundColor:"yellow",marginTop:"50px"}}>
+        <div style={{border:"3px solid green" , width:"300px", height:"200px", margin:"auto",borderRadius:"20px",alignItems:"center",backgroundColor:"red",marginTop:"45px"}}>
+          <h1 style={{marginLeft:"90px", marginTop:"70px"}}>Cybrpm</h1>
+        </div>
+      </div>
+    </div>
       
-//       </>
-//   )
-// }
-// export default App;
+      </>
+  )
+}
+export default App;
