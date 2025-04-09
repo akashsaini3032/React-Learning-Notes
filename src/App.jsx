656fa483ccaf386dab779abc5bc1e19c32648493
akashsaini3032 <akashsainira@gmail.com>
2025-04-09 10:08:33 +0530

    update content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-const App = ()=>{
-  return(
-      <>
-    <div style={{border:"3px solid red" , width:"600px", height:"400px", margin:"auto",borderRadius:"20px" ,color:"red",backgroundColor:"blue",color:"white"}}>
-      <div style={{border:"3px solid blue" , width:"400px", height:"300px", margin:"auto",borderRadius:"20px",alignItems:"center",backgroundColor:"yellow",marginTop:"50px"}}>
-        <div style={{border:"3px solid green" , width:"300px", height:"200px", margin:"auto",borderRadius:"20px",alignItems:"center",backgroundColor:"red",marginTop:"45px"}}>
-          <h1 style={{marginLeft:"90px", marginTop:"70px"}}>Cybrpm</h1>
-        </div>
-      </div>
-    </div>
-      
-      </>
-  )
-}
-export default App;
+// const Data={
+ 
+ //   color:"red"
+   
+ // }
+ 
+ 
+ // const App = ()=>{
+ //   return(
+ //       <>
+    
+ //       <h1 style={Data}>Welcome to cybrom</h1>
+ //       </>
+ //   )
+ // }
+ // export default App;
