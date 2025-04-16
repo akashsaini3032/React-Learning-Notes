7286312e6e49edf0b639f57d994335caa1c1c472
akashsaini3032 <akashsainira@gmail.com>
2025-04-16 22:14:57 +0530

    update content

--- src/App.jsx
+++ src/App.jsx
@@ -19,3 +19,29 @@
 // }
 
 // export default App;
+
+
+
+
+// import { useState } from "react";
+
+// import Comp1 from "./Comp1";
+
+
+
+// const App=()=>{
+//     const [user, SetUser]= useState("Akash");
+
+//     return(
+
+//         <>
+
+//         <h1>Welcome {user} !!!</h1>
+//         <Comp1 user={user} />
+        
+//         </>
+//     )
+// }
+
+// export default App;
+
