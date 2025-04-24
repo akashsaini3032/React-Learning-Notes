1bf1f5da9426df556e27e17d53c9dd4d69f54e23
akashsaini3032 <akashsainira@gmail.com>
2025-04-24 22:37:15 +0530

    update content

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 
 
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./Home";
-import Insert from "./Insert";
-import Display from "./Display";
-import Increment from "./Increment";
-import Theme from "./Theme";
+
 
 const App=()=>{
   return(
     <> 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path="home" element={<Home/>}/>
-            <Route path="insert" element={<Insert/>}/>
-            <Route path="display" element={<Display/>}/>
-            <Route path="increment" element={<Increment/>}/>
-            <Route path="theme" element={<Theme/>}/>
-           
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      
     </>
   )
 }
