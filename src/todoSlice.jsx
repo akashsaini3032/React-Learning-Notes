b299d5f52875ef369174dd80e343c38ae175e4d4
akashsaini3032 <akashsainira@gmail.com>
2025-05-03 22:56:24 +0530

    to-do app complete

--- src/todoSlice.jsx
+++ src/todoSlice.jsx
@@ -49,3 +49,50 @@
 // ╰════════════════════════════════════════════════════════════╯
 
 
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+    name:"todo",
+    initialState:{
+        task:[]
+    },
+
+    reducers:{
+        addTask:(state, actions)=>{
+            state.task.push(actions.payload);
+        },
+        removeTask:(state, actions)=>{
+            state.task=state.task.filter(key=>key.id!=actions.payload.id);
+        },
+        completeTask:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].status=true;
+                }
+            }
+        },
+        IncompleteTask:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].status=false;
+                }
+            }
+        },
+        editDataSave:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id== actions.payload.id)
+                {
+                    state.task[i].work= actions.payload.work;
+                }
+            }
+        }
+    }
+})
+
+export const {addTask, removeTask, completeTask, IncompleteTask, editDataSave} = todoSlice.actions;
+export default todoSlice.reducer;
