aa2b1098b642b95e5b66552da4bb8964fb2cdf3b
akashsaini3032 <akashsainira@gmail.com>
2025-05-04 22:29:07 +0530

    practice

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -3,4 +3,36 @@
 
 
 
+import { createSlice } from "@reduxjs/toolkit";
 
+const todoSlice=createSlice({
+    name:"todo",
+    initialState:{
+        task:[]
+    },
+
+
+    reducers:{
+        addTask:(state, actions)=>{
+            state.task.push(actions.payload);
+            console.log(actions);
+        },
+
+        removeTask:(state, actions)={
+
+            state.task=state.task.filter(item=>item.id!actions.payload.id)
+
+        },
+
+        remByIndex:(state, actions)=>{
+            state.task.splice(actions.payload.id,1 );
+            console.log(actions.payload.id);
+        }
+    }
+
+
+})
+
+
+export const {addTask, removeTask, remByIndex}=todoSlice.actions;
+export default todoSlice.reducer;
