9376c4beb51528911ec2367d8fe4410da1487501
akashsaini3032 <akashsainira@gmail.com>
2025-10-14 22:42:29 +0530

    react practice

--- src/Cybrom.jsx
+++ src/Cybrom.jsx
@@ -6,32 +6,32 @@
 
 
 
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 
-import { changeColor } from "./colorSlice";
-
-
-const Cybrom=()=>{
-
-    const clr= useSelector(state=>state.mycolor.color);
-    const dispatch= useDispatch();
-
-    return(
-        <>
-        <h1>Redux Toolkit</h1>
-        <h2>colour change</h2>
+// import { changeColor } from "./colorSlice";
 
 
-        <button onClick={()=>{dispatch(changeColor())}}>Change</button>
+// const Cybrom=()=>{
 
-        <div style={{width:"200px",height:"200px",backgroundColor:clr}}></div>
+//     const clr= useSelector(state=>state.mycolor.color);
+//     const dispatch= useDispatch();
+
+//     return(
+//         <>
+//         <h1>Redux Toolkit</h1>
+//         <h2>colour change</h2>
+
+
+//         <button onClick={()=>{dispatch(changeColor())}}>Change</button>
+
+//         <div style={{width:"200px",height:"200px",backgroundColor:clr}}></div>
         
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
 
-export default Cybrom;
+// export default Cybrom;
 
 
